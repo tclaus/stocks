e01bd249969b94aa3f763b5484b0d1ba9a35900e
Thorsten Claus <thorstenclaus@web.de>
2023-02-27 22:51:28 +0100

    Center text in chartview

--- gui/chartView/ChartView.cpp
+++ gui/chartView/ChartView.cpp
@@ -1,25 +1,22 @@
 
-/*
- * Copyright 2018, Your Name <your@email.address>
- * All rights reserved. Distributed under the terms of the MIT license.
- */
-
 #include "ChartView.h"
 #include <LayoutBuilder.h>
 #include <StringView.h>
 #include <View.h>
 
 ChartView::ChartView()
-    : BView(BRect(0, 0, 300, 200), "mainView", B_FOLLOW_ALL, B_WILL_DRAW) {
+        : BView(BRect(0, 0, 300, 200), "mainView", B_FOLLOW_ALL, B_WILL_DRAW) {
 
-  this->SetViewColor(ui_color(B_PANEL_BACKGROUND_COLOR));
+    this->SetViewColor(ui_color(B_PANEL_BACKGROUND_COLOR));
 
-  BStringView *placeholderText =
-      new BStringView("placeholderLabel", "Choose a share from the shares list");
+    BStringView *placeholderText =
+            new BStringView("placeholderLabel", "Choose a share from the shares list");
 
-  BLayoutBuilder::Group<>(this, B_VERTICAL)
-      .AddGrid()
-      .Add(placeholderText, 1, 1);
+    BLayoutBuilder::Group<>(this, B_VERTICAL)
+            .AddGrid()
+            .AddGlue(0, 1)
+            .Add(placeholderText, 1, 1)
+            .AddGlue(2, 1);
 }
 
 ChartView::~ChartView() = default;
