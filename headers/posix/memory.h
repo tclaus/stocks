0df38e505b3ec0a9326d11ec54cc7620243c96cc
Thorsten Claus <thorstenclaus@web.de>
2023-02-27 22:53:11 +0100

    Removing Haiku headers
    By remote developing the heders now come
    directly from heiku itself.

--- headers/posix/memory.h
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2005-2012 Haiku, Inc. All Rights Reserved.
- * Distributed under the terms of the MIT License.
- */
-
-
- #include <string.h>
-
--- headers/posix/wchar_t.h
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2008-2012 Haiku, Inc. All Rights Reserved.
- * Distributed under the terms of the MIT License.
- */
-
-
-/* Include GCC's stddef.h. It defines wchar_t. */
-#include <stddef.h>
