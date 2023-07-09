df135d1467b1ecd2d5296fc2624bc053cf1d551f
Thorsten Claus <thorstenclaus@web.de>
2023-07-09 23:38:16 +0200

    Adding build file

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 echo Building stocks
 cmake -DCMAKE_BUILD_TYPE=Release -S ./ -B build
 cmake --build ./build --target Stocks -- -j 4
+
+echo building package
+cd package
+cp ../build/Stocks ./
+package create -i ../.PackageInfo Stocks-x86
