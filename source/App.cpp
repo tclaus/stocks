6680623a9a9ab16f3ea0ba06d634817abd179c53
Thorsten Claus <thorstenclaus@web.de>
2023-07-09 23:10:51 +0200

    Adding error if build without api key

--- source/App.cpp
+++ source/App.cpp
@@ -3,6 +3,7 @@
 
 #include "App.h"
 #include "gui/MainWindow.h"
+#include "FinancialmodelingApiKey.h"
 
 App::App() : BApplication("application/x-vnd.tclaus-StocksApp") {
     window = new MainWindow();
@@ -10,10 +11,21 @@ App::App() : BApplication("application/x-vnd.tclaus-StocksApp") {
 }
 
 int main() {
+    App::checkForApiKey();
+
     App *app = new App();
     app->Run();
     delete app;
     return 0;
+}
+
+void
+App::checkForApiKey() {
+    if (FinancialmodelingApiKey::GetApiKey().empty()) {
+        const char *no_api_key_set = "No API key set in build. Rebuild app with API key set in the environment. "
+                                     "Have a look in the readme file.";
+        throw std::invalid_argument(no_api_key_set);
+    }
 }
 
 #endif // APP_CPP_
