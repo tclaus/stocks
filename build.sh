49f3467e7623ba634257bdc099e7e50e6da95d02
Thorsten Claus <thorstenclaus@web.de>
2023-08-01 00:17:08 +0200

    Added symbolic link to Stocks app in build.sh script

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ cmake --build ./build --target Stocks -- -j 4
 echo building package
 cd package
 cp ../build/Stocks ./
+ln -rs /boot/system/apps/Stocks/stocks  /Stocks/stocks
 package create -i ../.PackageInfo Stocks-0.1-x86.hpkg
