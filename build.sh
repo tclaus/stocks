c83e4b620abc7bb1c78c0b41b06fce30f20968d3
Thorsten Claus <ThorstenClaus@web.de>
2023-07-31 23:39:04 +0200

    Adapt changes in build and package file

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ cmake -DCMAKE_BUILD_TYPE=Release -S ./ -B build
 cmake --build ./build --target Stocks -- -j 4
 
 echo building package
+mkdir -p package/apps
 cd package
-cp ../build/Stocks ./
-package create -i ../.PackageInfo Stocks-0.1-x86.hpkg
+cp ../build/Stocks ./apps
+package create -v -i ../.PackageInfo ../Stocks-0.1-x86.hpkg
