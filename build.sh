f592efaa7ddb538a6f00e76d6357b3edf1c54680
Thorsten Claus <ThorstenClaus@web.de>
2023-08-02 08:57:55 +0200

    Builing package with deskbar link

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ echo Building stocks
 cmake -DCMAKE_BUILD_TYPE=Release -S ./ -B build
 cmake --build ./build --target Stocks -- -j 4
 
-echo building package
-mkdir -p package/apps
+echo Building package
+
 cd package
 cp ../build/Stocks ./apps
 package create -v -i ../.PackageInfo ../stocks-0.1-x86_64.hpkg
